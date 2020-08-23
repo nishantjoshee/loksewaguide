87782d339660e6a2b8a47db9910c6dff6d3c5d95
Nishant <nishantjoshee@gmail.com>
2020-08-23 13:22:22 +0545

    Started Application from Scratch

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Loksewa Guide'),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+
+            ],
+          ),
+          Row(
+            children: [
+
+            ],
+          ),
+          Row(
+            children: [
+
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
