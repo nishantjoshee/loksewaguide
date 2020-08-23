a542edd2f7884230b9c69e5c5e3a63dd8bd6db56
Nishant <nishantjoshee@gmail.com>
2020-08-23 15:35:53 +0545

    implemented listview

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,16 +11,52 @@ class _HomePageState extends State<HomePage> {
 
   Widget customcard(){
     return Padding(
-      padding: EdgeInsets.all(10),
+      padding: EdgeInsets.all(20),
+      child: InkWell(
+        child: Material(
+          elevation: 10,
+          child: Container(
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 10,
+                  ),
+                  child: Material(
+                    child: Container(
+                      child: Text("Universe"),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding:EdgeInsets.symmetric(
+                    vertical: 10,
+                  ) ,
+                  child: Material(
+                    child: Container(
+                      child: Text("Sets: 10"),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
 
   }
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        customcard(),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Loksewa Guide"),
+      ),
+      body: ListView(
+        children: [
+          customcard(),
+        ],
+      ),
     );
   }
 }
