d8b6dd80ecfc4b46c429faac05255271170d6e70
Nishant <nishantjoshee@gmail.com>
2020-08-31 23:33:37 +0545

    deleted some files

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:loksewaguide/quizList.dart';
-import 'package:loksewaguide/secondPage.dart';
+
 
 class SplashScreen extends StatefulWidget {
   @override
